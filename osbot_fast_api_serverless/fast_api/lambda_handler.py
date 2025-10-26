54e3458c74f3c602b3f8f20a846dddde70350373
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-26 14:11:05 +0000

    fixed lambda deployment

--- osbot_fast_api_serverless/fast_api/lambda_handler.py
+++ osbot_fast_api_serverless/fast_api/lambda_handler.py
@@ -1,6 +1,6 @@
 from osbot_aws.aws.lambda_.boto3__lambda import load_dependencies       # using the lightweight file (which only has the boto3 calls required to load_dependencies)
 
-LAMBDA_DEPENDENCIES =  ['osbot-fast-api==v0.28.0', 'mangum']
+LAMBDA_DEPENDENCIES =  ['osbot-fast-api==v0.31.0', 'mangum==0.19.0']
 
 load_dependencies(LAMBDA_DEPENDENCIES)
 
